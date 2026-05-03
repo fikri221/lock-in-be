0be0823ce94617b0c5c906d7f96cce959900ae25
Fikri Lazuardi <fikrim69@gmail.com>
2026-05-03 23:13:35 +0700

    chore: update rate limit window and modify CORS origin

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(cors({
 
 // Rate limiting middleware
 const apiLimiter = rateLimit({
-  windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS) || 15 * 60 * 1000, // 15 minutes
+  windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS) || 60 * 1000, // 1 minutes
   max: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS) || 100, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later."
 });
